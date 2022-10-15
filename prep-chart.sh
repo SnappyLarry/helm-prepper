37b83b323c8e01bdf231ddd2cd9ddd17ceca5fca
Perlin <lgauthier541@outlook.com>
2022-10-14 22:43:13 -0400

    added the tag

--- prep-chart.sh
+++ prep-chart.sh
@@ -5,9 +5,9 @@ helmChartUrl="https://argoproj.github.io/argo-helm"
 helmChartVersion="5.6.0"
 
 
-helm repo add $helmChartName $helmChartUrl
-helm pull $helmChartName/$helmChartName --version $helmChartVersion
-tar xvfz $helmChartName-$helmChartVersion.tgz
+# helm repo add $helmChartName $helmChartUrl
+# helm pull $helmChartName/$helmChartName --version $helmChartVersion
+# tar xvfz $helmChartName-$helmChartVersion.tgz
 
 
 create_list(){
@@ -22,13 +22,17 @@ create_value_file(){
   echo "---" > generated_values.yaml
   while read i; do
     imageRepo=${i%:*}
+    imageTag=${i#*:}
     yamlPath=$(yq '.. | select(. == '\"$imageRepo\"') | path' $helmChartName/values.yaml \
       | sed 's/-//; s/ //; s/^/./' \
       | tr -d '\n')
-    yq -n '('$yamlPath' = '\"$imageRepo\"')' >> generated_values.yaml
+    yamlPathRootKey=$(cut -d '.' -f 2 <<< $yamlPath )
+    yq -n '('$yamlPath' = '\"$imageRepo\"')' > temp
+    cat temp | yq .$yamlPathRootKey'.image += {"tag": '\"$imageTag\"'}' >> generated_values.yaml
+    rm -rf temp
   done < image-list.txt
 }
 
 
-create_list
+# create_list
 create_value_file
